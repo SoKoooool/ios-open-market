5533e143012c2c89d80101159994f5a1d48e656a
stevenkim18 <11882@naver.com>
2021-05-14 15:23:46 +0900

    feat: 요구사항과 거의 비슷하게 segment Control 구현

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -7,12 +7,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var segmentedController: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setSegemetedController()
+    }
+    
+    
+    func setSegemetedController () {
+        let items = ["List", "GRID"]
+        segmentedController = UISegmentedControl(items: items)
+        segmentedController.tintColor = UIColor.white
+        segmentedController.layer.borderColor = UIColor.systemBlue.cgColor
+        segmentedController.backgroundColor = UIColor.systemBackground
+        segmentedController.selectedSegmentIndex = 0
+        segmentedController.selectedSegmentTintColor = UIColor.systemBlue
+        let selectedTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        segmentedController.setTitleTextAttributes(selectedTextAttributes, for: .selected)
+        let normalTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
+        segmentedController.setTitleTextAttributes(normalTextAttributes, for: .normal)
+        segmentedController.layer.borderWidth = 1
+        for index in 0..<items.count {
+            segmentedController.setWidth(80, forSegmentAt: index)
+        }
+        navigationItem.titleView = segmentedController
     
     }
-
 
 }
 
