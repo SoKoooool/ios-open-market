ce4fcba21083e3a83356f3760979fe53d5893c00
stevenkim18 <11882@naver.com>
2021-05-27 14:49:44 +0900

    feat: segmentedControl 요구사항에 맞게 구현

--- OpenMarket/OpenMarket/Controller/ViewController.swift
+++ OpenMarket/OpenMarket/Controller/ViewController.swift
@@ -8,11 +8,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewTypeSegmentControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     
+        initSegmentControl()
     }
-
+    
+    func initSegmentControl() {
+        viewTypeSegmentControl.setTitle("LIST", forSegmentAt: 0)
+        viewTypeSegmentControl.setTitle("GRID", forSegmentAt: 1)
+        viewTypeSegmentControl.tintColor = UIColor.white
+        viewTypeSegmentControl.layer.borderColor = UIColor.systemBlue.cgColor
+        viewTypeSegmentControl.backgroundColor = UIColor.systemBackground
+        viewTypeSegmentControl.selectedSegmentTintColor = UIColor.systemBlue
+        let selectedTextAttrubutes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        viewTypeSegmentControl.setTitleTextAttributes(selectedTextAttrubutes, for: .selected)
+        let normalTextAttrubutes = [NSAttributedString.Key.foregroundColor: UIColor.systemBlue]
+        viewTypeSegmentControl.setTitleTextAttributes(normalTextAttrubutes, for: .normal)
+        viewTypeSegmentControl.layer.borderWidth = 1
+    }
 
 }
 
