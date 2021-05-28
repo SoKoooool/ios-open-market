494712b12ac08fdffe796d587c0cd56c911643b6
stevenkim18 <11882@naver.com>
2021-05-28 15:48:36 +0900

    feat: 테이블뷰 셀 오토레이아웃 적용

--- OpenMarket/OpenMarket/Controller/ListViewController.swift
+++ OpenMarket/OpenMarket/Controller/ListViewController.swift
@@ -17,22 +17,35 @@ class ListViewController: UIViewController {
         
         productTableView.delegate = self
         productTableView.dataSource = self
+        
+        productTableView.register(UINib(nibName: "ProductTableViewCell", bundle: nil), forCellReuseIdentifier: "ProductTableViewCell")
     }
 
 }
 
 extension ListViewController: UITableViewDelegate {
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60
+    }
 }
 
 extension ListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ProductTableViewCell", for: indexPath) as? ProductTableViewCell else {
+            return UITableViewCell()
+        }
+        
+        cell.titleLabel.text = "케인케인케인케인케인케인"
+        cell.priceLabel.text = "KRW 1200000000"
+        cell.discountedPriceLabel.text = "KRW 100000"
+        cell.stockLabel.text = "잔여수량 : 148"
+        cell.thumbnail.image = UIImage(named: "kane")
+        
+        return cell
     }
-    
     
 }
